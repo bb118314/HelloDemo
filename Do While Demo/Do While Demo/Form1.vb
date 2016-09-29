b2253ff5f232c22a92e177ae95e80931abe5da84
bb118314 <roflwhy@gmail.com>
2016-09-29 11:06:46 -0500

    Add files via upload

---
+++ Do While Demo/Do While Demo/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intCount As Integer = 0
+
+        Do
+            ListBox1.Items.Add("Hello")
+            intCount += 1
+        Loop While intCount < 10
+    End Sub
+End Class
